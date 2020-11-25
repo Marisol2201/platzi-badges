a7a2250c63b14d38be601e2cb8a89a44dca13569
Marisol2201 <1529@holbertonschool.com>
2020-11-25 16:51:54 -0500

    Adding pages and components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import './global.css';
+import BadgeNew from './pages/BadgeNew';
+//import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<BadgeNew />, container);
+
+/* ReactDOM.render(<Badge
+    firstName="Marisol"
+    lastName="Ramírez Henao"
+    avatarUrl="https://lh3.googleusercontent.com/ogw/ADGmqu9PS2RWru89tPwTmj35yXOKAGhUjjsbyuON8YQtNg=s83-c-mo"
+    jobTitle="Frontend engineer"
+    twitter="MarisolRHe"
+    />,
+    container); */
